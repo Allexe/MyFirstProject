9aad01b7b09dc5e97e1a004f7049dd548417ccee
Vorotnikov_AV <vorotnikov_av@toyota-l.ru>
2012-10-29 17:00:24 +0400

    11 Немного подправил CarOuner.java & JBCar.java

--- src/CarOwner.java
+++ src/CarOwner.java
@@ -4,9 +4,10 @@ public class CarOwner {
 	public static void main(String[] args) {
 		int time = 2; // это время в часах
 		Car myCar = new Car();
+		//JBCar myCar = new JBCar();
 		myCar.start();
 		myCar.stop();
-		System.out.println("Мы проехали " + myCar.drive(time) + "км со средней скоростью " + time + "км/ч.");
+		System.out.println("Мы проехали " + myCar.drive(time) + "км за " + time + " часа!");
 	}
 
 }
--- src/JBCar.java
+++ src/JBCar.java
@@ -4,6 +4,7 @@ public class JBCar extends Car {
 	
 	public int drive(int howlong) {
 		int distance = howlong*180;
+		System.out.println("За рулём был сам Джеймс Бонд!");
 		return distance;
 	}
 	//int distance = howlong*180;
